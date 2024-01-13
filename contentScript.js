95e5501722c6da4947f34cc4a876f6c1f3968cb8
Joshua Nguyen <joshuanguyen@joshuas-mbp.thenguyen.com>
2024-01-13 18:05:28 -0500

    Functional course links

--- contentScript.js
+++ contentScript.js
@@ -1,15 +1,22 @@
 (() => {
     chrome.runtime.onMessage.addListener((obj, sender, response) => {
         courses = document.getElementsByClassName("course-id");
+        // console.log(courses[0].textContent)
 
         for(let i = 0; i < courses.length; i++) {
-            const reviewsBtn = document.createElement("img");
-            reviewsBtn.src = chrome.runtime.getURL("assets/logo.png");
-            reviewsBtn.className = "reviews-btn";
-            reviewsBtn.title = "Click to view student reviews";
-            reviewsBtn.style.width = "40%";
+            const anchorElem = document.createElement("a");
+            anchorElem.href = "https://planetterp.com/course/" + courses[i].textContent;
+            anchorElem.target = "_blank";
+            anchorElem.className = "reviews-btn";
+            anchorElem.title = "View student reviews/details";
 
-            courses[i].appendChild(reviewsBtn);
+            const imgElem = document.createElement("img");
+            imgElem.src = chrome.runtime.getURL("assets/logo.png");
+            imgElem.alt = "PlanetTerp";
+            imgElem.style.width = "40%";
+
+            anchorElem.appendChild(imgElem);
+            courses[i].appendChild(anchorElem);
         }
     });
 })();
